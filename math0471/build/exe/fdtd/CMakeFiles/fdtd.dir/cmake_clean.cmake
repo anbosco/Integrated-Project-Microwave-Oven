10b8cf4ba531a9bd997f0410c62556861aecade3
MichelHoubart <36197576+MichelHoubart@users.noreply.github.com>
2018-02-27 13:24:41 +0100

    Add files via upload

---
+++ math0471/build/exe/fdtd/CMakeFiles/fdtd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fdtd.dir/fdtd.cpp.o"
+  "../../bin/fdtd.pdb"
+  "../../bin/fdtd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fdtd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ math0471/build/vtl/CMakeFiles/vtl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vtl.dir/vtlSPoints.cpp.o"
+  "CMakeFiles/vtl.dir/vtl.cpp.o"
+  "../bin/libvtl.pdb"
+  "../bin/libvtl.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vtl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
