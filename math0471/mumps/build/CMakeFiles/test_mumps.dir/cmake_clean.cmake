fa3b83c20fbb3e38cac35de4816ca23ad472f4f8
Michel <michel.houbart@hotmail.com>
2018-03-13 15:25:33 +0100

    Add MUMPS

---
+++ math0471/mumps/build/CMakeFiles/test_mumps.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_mumps.dir/laplace.o"
+  "CMakeFiles/test_mumps.dir/main.o"
+  "test_mumps.pdb"
+  "test_mumps"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_mumps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
