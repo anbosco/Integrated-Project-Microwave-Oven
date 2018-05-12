5160136449d74b2c3819fa33b6887819f6b75c09
anbosco <anthony.bosco@student.uliege.be>
2018-05-12 14:16:34 +0200

    Updated phase change + materials

--- TestDir/Solver/material.cpp
+++ TestDir/Solver/material.cpp
@@ -20,15 +20,16 @@ Material ReturnMaterial;
 
 
 switch(Val){
+
   case 0:
   {
   // Air
   Material air;
   air.name = "air";
-  air.rho = {1, 1};
+  air.rho = {1.2, 1.2};
   air.id = 0;
-  air.k = {1, 1};
-  air.cp = {1, 1};
+  air.k = {0.025, 0.025};
+  air.cp = {1004, 1004};
   air.TempPhaseChange = {0};
   air.er = {1, 1};
   air.ur = {1, 1};
@@ -42,9 +43,15 @@ switch(Val){
   {
   // Water
   Material water;
-  water.name = "water";
+  water.name = "Water";
+  water.rho = {995, 996, 997, 998, 999 , 999.84 , 999.9, 999.9, 999.9, 999.9, 999.5, 999 ,997 };
   water.id = 1;
-  // etc
+  water.k = {2.3, 2.2, 2.1, 2, 1.9, 0.569, 0.572, 0.574, 0.578, 0.582, 0.586, 0.595, 0.6};
+  water.cp = {2000, 2005, 2010, 2015, 2020, 4217, 4214, 4210, 4205, 4200, 4195, 4185, 4180};
+  water.TempPhaseChange = {-4,-3,-2,-1, 0, 1 , 2, 3, 4, 5, 10, 20};
+  water.er = {3.15, 3.16, 3.17, 3.18, 3.19, 60, 65, 70, 75.8, 75.8, 75.8, 75.8, 75.8};
+  water.ur = {2.3, 2.2, 2.1, 2, 1.9, 0.569, 0.572, 0.574, 0.578, 0.582, 0.586, 0.595, 0.6};
+  water.eDiel = {0.001, 0.002, 0.003, 0.004, 0.005, 3, 3.5, 4, 4.5, 5, 5, 5, 5};
   ReturnMaterial = water;
 }
   break;
@@ -64,6 +71,24 @@ switch(Val){
   FoodFromRef.ur = {1, 1, 1};
   FoodFromRef.eDiel = {1, 1, 1};
   ReturnMaterial = FoodFromRef;
+}
+  break;
+
+
+  case 3:
+  {
+   // Some food from a reference.
+  Material Alpha;
+  Alpha.name = "Alpha";
+  Alpha.rho = {1, 1};
+  Alpha.id = 3;
+  Alpha.k = {1, 1};
+  Alpha.cp = {1, 1};
+  Alpha.TempPhaseChange = {0};
+  Alpha.er = {1, 1};
+  Alpha.ur = {1, 1};
+  Alpha.eDiel = {1, 1};
+  ReturnMaterial = Alpha;
 }
   break;
 
