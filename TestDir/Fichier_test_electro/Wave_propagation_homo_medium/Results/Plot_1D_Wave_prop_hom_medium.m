0a198cfc32e474aef4ba5ad24b9298a0cb92a6f0
Michel Houbart (michel.houbart@student.ulg.ac.be) <michel.houbart@hotmail.com>
2018-05-15 18:10:58 +0200

    Param files in Test dir

---
+++ TestDir/Fichier_test_electro/Wave_propagation_homo_medium/Results/Plot_1D_Wave_prop_hom_medium.m
@@ -0,0 +1,110 @@
+%% This function plot the results for the 1D wave propagation in a homogeneous
+% medium
+function plot_impulse_evo
+dx = 0.005
+x=0:dx:0.6;
+x = transpose(x);       % To plot Ey
+x2 = 0:dx:0.6+dx;       % To plot Hz or Hx
+x2 = transpose(x2);
+
+cd Cut 
+%% Importation
+%% Ey
+load('Cut_Ey_alongX_step25_rank0.txt');
+load('Cut_Ey_alongX_step25_rank4.txt');
+y1_Ey = [Cut_Ey_alongX_step25_rank0; Cut_Ey_alongX_step25_rank4];
+
+load('Cut_Ey_alongX_step50_rank0.txt');
+load('Cut_Ey_alongX_step50_rank4.txt');
+y2_Ey = [Cut_Ey_alongX_step50_rank0; Cut_Ey_alongX_step50_rank4];
+
+load('Cut_Ey_alongX_step75_rank0.txt');
+load('Cut_Ey_alongX_step75_rank4.txt');
+y3_Ey = [Cut_Ey_alongX_step75_rank0; Cut_Ey_alongX_step75_rank4];
+
+load('Cut_Ey_alongX_step100_rank0.txt');
+load('Cut_Ey_alongX_step100_rank4.txt');
+y4_Ey = [Cut_Ey_alongX_step100_rank0; Cut_Ey_alongX_step100_rank4];
+
+load('Cut_Ey_alongX_step150_rank0.txt');
+load('Cut_Ey_alongX_step150_rank4.txt');
+y5_Ey = [Cut_Ey_alongX_step150_rank0; Cut_Ey_alongX_step150_rank4];
+
+%% Hx
+load('Cut_Hx_alongX_step25_rank0.txt');
+load('Cut_Hx_alongX_step25_rank4.txt');
+y1_Hx = [Cut_Hx_alongX_step25_rank0; Cut_Hx_alongX_step25_rank4];
+
+load('Cut_Hx_alongX_step50_rank0.txt');
+load('Cut_Hx_alongX_step50_rank4.txt');
+y2_Hx = [Cut_Hx_alongX_step50_rank0; Cut_Hx_alongX_step50_rank4];
+
+load('Cut_Hx_alongX_step75_rank0.txt');
+load('Cut_Hx_alongX_step75_rank4.txt');
+y3_Hx = [Cut_Hx_alongX_step75_rank0; Cut_Hx_alongX_step75_rank4];
+
+load('Cut_Hx_alongX_step150_rank0.txt');
+load('Cut_Hx_alongX_step150_rank4.txt');
+y5_Hx = [Cut_Hx_alongX_step150_rank0; Cut_Hx_alongX_step150_rank4];
+
+%% Hz
+load('Cut_Hz_alongX_step25_rank0.txt');
+load('Cut_Hz_alongX_step25_rank4.txt');
+y1_Hz = [Cut_Hz_alongX_step25_rank0; Cut_Hz_alongX_step25_rank4];
+
+load('Cut_Hz_alongX_step50_rank0.txt');
+load('Cut_Hz_alongX_step50_rank4.txt');
+y2_Hz = [Cut_Hz_alongX_step50_rank0; Cut_Hz_alongX_step50_rank4];
+
+load('Cut_Hz_alongX_step75_rank0.txt');
+load('Cut_Hz_alongX_step75_rank4.txt');
+y3_Hz = [Cut_Hz_alongX_step75_rank0; Cut_Hz_alongX_step75_rank4];
+
+load('Cut_Hz_alongX_step150_rank0.txt');
+load('Cut_Hz_alongX_step150_rank4.txt');
+y5_Hz = [Cut_Hz_alongX_step150_rank0; Cut_Hz_alongX_step150_rank4];
+cd ..
+
+%% Plot of the results
+Figure1=figure(1);clf;set(Figure1,'defaulttextinterpreter','latex');
+hold on;
+subplot(2,2,1)
+set(gca,'fontsize',150,'fontname','Times','LineWidth',0.5);
+plot(x,y1_Ey,'r','linewidth',2);
+xlabel('$x [m]$');
+ylabel('$E_y [V\cdot m^{-1}]$');
+ylim([-150 150]);
+box on;
+grid on;
+
+subplot(2,2,2)
+set(gca,'fontsize',80,'fontname','Times','LineWidth',0.5);
+plot(x,y2_Ey,'r','linewidth',2);
+xlabel('$x [m]$');
+ylabel('$E_y [V\cdot m^{-1}]$');
+ylim([-150 150]);
+box on;
+grid on;
+
+subplot(2,2,3)
+set(gca,'fontsize',80,'fontname','Times','LineWidth',0.5);
+plot(x,y4_Ey,'r','linewidth',2);
+xlabel('$x [m]$');
+ylabel('$E_y [V\cdot m^{-1}]$');
+ylim([-150 150]);
+box on;
+grid on;
+
+subplot(2,2,4)
+hold on
+set(gca,'fontsize',80,'fontname','Times','LineWidth',0.5);
+plot(x,y5_Ey,'r','linewidth',2);
+plot([0.525 0.525], [-150 150],'k-.','linewidth',2)
+plot([0.075 0.075], [-150 150],'k-.','linewidth',2)
+xlabel('$x [m]$');
+ylabel('$E_y [V\cdot m^{-1}]$');
+ylim([-150 150]);
+box on;
+grid on;
+set(findall(gcf,'-property','FontSize'),'FontSize',30)
+end
--- TestDir/Fichier_test_thermo/Test_order_temporal/results/theta_0_5/Test_order_temp.m
+++ TestDir/Fichier_test_thermo/Test_order_temporal/results/theta_0_5/Test_order_temp.m
@@ -1,9 +1,9 @@
 function Test_order_temp
 close all
 clear all
-% cd dt_1
-% Temperature1 = load('Probe1_step0_to_step100.txt');
-% cd ..
+cd dt_1
+Temperature1 = load('Probe1_step0_to_step100.txt');
+cd ..
 
 cd dt_0_1
 Temperature2 = load('Probe1_step0_to_step1000.txt');
@@ -27,7 +27,7 @@ t2 = 0:dt2:100-dt2;
 t3 = 0:dt3:100-dt3;
 t4 = 0:dt4:100-dt4;
 hold on;
-% plot(t1,Temperature1,'r.','Markersize',15);
+plot(t1,Temperature1,'r.','Markersize',15);
 plot(t2,Temperature2,'r.','Markersize',15);
 plot(t3,Temperature3,'k.','Markersize',15);
 
@@ -35,12 +35,12 @@ Q = 50000;
 rho = 1;
 cp = 1;
 omega = (2*3.141592)/20;
-T_anal1 = (Q/(rho*cp*omega))*(1-cos(omega*t1));
-T_anal2 = (Q/(rho*cp*omega))*(1-cos(omega*t2));
-T_anal3 = (Q/(rho*cp*omega))*(1-cos(omega*t3));
-T_anal4 = (Q/(rho*cp*omega))*(1-cos(omega*t4));
+T_anal1 = (Q/(rho*cp*omega))*sin(omega*t1);
+T_anal2 = (Q/(rho*cp*omega))*sin(omega*t2);
+T_anal3 = (Q/(rho*cp*omega))*sin(omega*t3);
+T_anal4 = (Q/(rho*cp*omega))*sin(omega*t4);
 
-% err1 = max(abs(transpose(T_anal1)-Temperature1))
+err1 = max(abs(transpose(T_anal1)-Temperature1))
 err2 = max(abs(transpose(T_anal2)-Temperature2))
 err3 = max(abs(transpose(T_anal3)-Temperature3))
 err4 = max(abs(transpose(T_anal4)-Temperature4))
